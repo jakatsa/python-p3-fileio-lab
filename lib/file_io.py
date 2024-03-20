83f2f7f99d52a12e2e9cfa582ed51418820f5b68
Joe <jakatsa5@gmail.com>
2024-03-20 12:53:38 +0300

    completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,3 +1,4 @@
+#function
 def write_file(file_name, file_content):
     with open(f'{file_name}.txt', 'w') as f:
         f.write(file_content)
@@ -9,3 +10,4 @@ def append_file(file_name, append_content):
 def read_file(file_name):
     with open(f'{file_name}.txt') as f:
         return f.read()
+   
